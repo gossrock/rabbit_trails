5a30b6931cdff67c46a7f3e32dd83da8293f6f5b
gossrock <gossrock@gmail.com>
2017-11-16 14:19:47 +0000

    added a couple more things to che curses_panels.py rabbit trail 1) made sure I could get keyboard input 2) made played around with hide and show on a panel

--- curses_panels.py
+++ curses_panels.py
@@ -8,12 +8,14 @@ from time import sleep
 import curses, curses.panel
 
 def make_panel(h,l, y,x, str):
-	win = curses.newwin(h,l, y,x)
-	win.erase()
-	win.box()
+	borderwin = curses.newwin(h,l, y,x)
+	borderwin.erase()
+	borderwin.box()
+	borderwin.noutrefresh()
+	win = borderwin.derwin(h-2, l-2, 1,1)
 	win.addstr(2, 2, str)
 
-	panel = curses.panel.new_panel(win)
+	panel = curses.panel.new_panel(borderwin)
 	return win, panel
 
 
@@ -37,10 +39,23 @@ def test(stdscr):
 	sleep(1)
 
 	for i in range(20):
+		try:
+			ch = stdscr.get_wch()
+		except:
+			ch = str(i)
+		ch = str(ch)
+		win1.addstr(ch)
+		win2.addstr(ch)
+		
+		if i%2 == 0:
+			panel1.hide()
+		else:
+			panel1.show()
+		
 		panel2.move(8, 8+i)
 		curses.panel.update_panels()
 		stdscr.refresh()
-		sleep(0.1)
+		sleep(0.5)
 
 	sleep(1)
 
